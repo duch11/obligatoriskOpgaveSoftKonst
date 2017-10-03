858c0e56beae8e15fe7693ed710bb37e74addc9d
Jonas Ladefoged Holm <jonas@holm.tech>
2017-10-03 11:36:45 +0200

    added all

---
+++ src/main/java/dk/kea/obligatoriskopgave/ObligatoriskOpgaveApplication.java
@@ -0,0 +1,12 @@
+package dk.kea.obligatoriskopgave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObligatoriskOpgaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObligatoriskOpgaveApplication.class, args);
+	}
+}
